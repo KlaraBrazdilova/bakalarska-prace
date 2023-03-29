442de275f28184a331ac27d44b1eff533127dd1c
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-03-29 11:32:49 +0200

    alterning-progress

--- alternating.py
+++ alternating.py
@@ -5,24 +5,31 @@ M = np.zeros(12,10)
 
 M = np.matrix(M)
 
-A = M
+A = np.random.permutation(M.shape[1])
 no_of_iterations = 20
 
 for i in range(no_of_iterations):
-    [m,n] = A.shape
-    W = A
+    #sloupce náhodně, potom utřídím řádky, transponuji matici a opakuji
+    
+    A = A[:, A[0, :].argsort()]
+    A = A.transpose()
 
-    for i in W:
-        if i == 0:
-            i = -1
 
-    tosort = []
+# for i in range(no_of_iterations):
+#     [m,n] = A.shape
+#     W = A
 
-    for i in range(m):
-        x = max_sub_array(W[i,:])
-        tosort[i,0] = x(0)
-        tosort[i,1] = x(1)
+#     for i in W:
+#         if i == 0:
+#             i = -1
 
-    [np.sort(tosort), np.perm]
-    A = A[np.perm, :]
-    A = A[:, np.perm]
+#     tosort = []
+
+#     for i in range(m):
+#         x = max_sub_array(W[i,:])
+#         tosort[i,0] = x(0)
+#         tosort[i,1] = x(1)
+
+#     [np.sort(tosort), np.perm]
+#     A = A[np.perm, :]
+#     A = A[:, np.perm]
