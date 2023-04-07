21387e55da52a427ba29cc19a45a2b2e78da7d74
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-04-07 11:02:00 +0200

    stable sort

--- matrix_similarity.py
+++ matrix_similarity.py
@@ -3,7 +3,8 @@ import numpy as np
 def matrix_similarity(A: np.matrix, B: np.matrix) -> np.matrix:
     """Compute coverage quality of matrix A and matrix B."""
     
-    sum1 = np.sum(A-B) #absolutní hodnotu přidat
-    sum2 = np.sum(A)
+    sum1 = np.sum(np.abs(A-B))     
+    sum2 = np.sum(np.abs(A))
     similarity = sum1/sum2
+
     return similarity
