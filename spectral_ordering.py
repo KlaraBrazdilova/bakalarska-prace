d8a87cc676e21b6ce86b1492c1ef116363e614ba
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-04-08 12:29:49 +0200

    final cleaning fix

--- spectral_ordering.py
+++ spectral_ordering.py
@@ -12,20 +12,20 @@ def spectral_ordering(M: np.matrix) -> np.matrix:
     for i in range(n):
         for j in range(i, n):
             # Pearson's coeficient
-            # pom = np.corrcoef(Q[:, i], Q[:, j])[0][1]
-            # S[i,j] = (1 + pom)/2 
-            # S[j,i] = (1 + pom)/2 
+            pom = np.corrcoef(Q[:, i], Q[:, j])[0][1]
+            S[i,j] = (1 + pom)/2 
+            S[j,i] = (1 + pom)/2 
             
             # Jaccard's coeficient
-            x = Q[:,i]
-            y = Q[:,j]
-            union = sum(x) + sum(y)
-            intersection = 0
-            for k in range(m):
-                if x[k] and y[k]:
-                    intersection += 1
-            S[i,j] = intersection / union
-            S[j,i] = intersection / union
+            # x = Q[:,i]
+            # y = Q[:,j]
+            # union = sum(x) + sum(y)
+            # intersection = 0
+            # for k in range(m):
+            #     if x[k] and y[k]:
+            #         intersection += 1
+            # S[i,j] = intersection / union
+            # S[j,i] = intersection / union
 
     #Laplacian matrix
     L = np.diag(np.diag(S)) - S
@@ -34,7 +34,7 @@ def spectral_ordering(M: np.matrix) -> np.matrix:
     D, V = np.linalg.eigh(L)
     
     a = np.argsort(D) 
-    perm = np.argsort(V[:,a[1]], kind='stable')
+    perm = np.flip(np.argsort(V[:,a[1]], kind='stable'))
     A = M[:, perm]
 
     return A
