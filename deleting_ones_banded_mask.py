37b6f64fa34987978f49c900a9cd267b1c88ff70
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-04-12 13:57:22 +0200

    deleting zoo

--- deleting_ones_banded_mask.py
+++ deleting_ones_banded_mask.py
@@ -1,6 +1,5 @@
-from matplotlib import cm, pyplot as plt
+from matplotlib import pyplot as plt
 import matplotlib
-from matplotlib.colors import ListedColormap
 import numpy as np
 import copy
 from banded_matrix import banded_matrix, banded_distance
@@ -12,15 +11,16 @@ def banded_ones(M: np.matrix, k: int) -> np.matrix:
     for i in range(final.shape[0]):
         for j in range(final.shape[1]):
             if not band[i,j] and final[i,j]:
-                final[i,j] = 0.1
+                final[i,j] = 2
     return band, final
 
 M = np.loadtxt("data/zoo/alternating.csv",
                  delimiter=",", dtype=int)
 vstup = copy.deepcopy(M)
-band, deleted = banded_ones(M, 50)  
+band, deleted = banded_ones(M, 70)  
+print(deleted)
 
-newcmp = matplotlib.colors.LinearSegmentedColormap.from_list("", [(0, 'white'), (0.1, 'blue'), (1,'black')])
+newcmp = matplotlib.colors.LinearSegmentedColormap.from_list("", ['white','black', 'blue'])
 
 fig, axs = plt.subplots(1, 3, figsize=(10, 5))
 axs[0].imshow(vstup, cmap=newcmp)
@@ -34,3 +34,10 @@ axs[2].set_title('Deleting')
 
 
 plt.show()
+
+for i in range(deleted.shape[0]):
+    for j in range(deleted.shape[1]):
+        if deleted[i,j] == 2:
+            deleted[i,j] = 0
+
+np.savetxt("data/zoo/alternating-deleted-band-70.csv", deleted, delimiter=",")         
