435e2d34612354518183ac375714b26f1825a346
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-04-14 14:41:41 +0200

    deleting outputs

--- deleting_ones_banded_mask.py
+++ deleting_ones_banded_mask.py
@@ -14,32 +14,36 @@ def banded_ones(M: np.matrix, k: int) -> np.matrix:
                 final[i,j] = 2
     return band, final
 
-M = np.loadtxt("data/paleo/alternating.csv",
-                 delimiter=",", dtype=int)
-vstup = copy.deepcopy(M)
-band, deleted = banded_ones(M, 90)  
-print(deleted)
+for i in [30, 50, 70, 90]:
+    slozka = "mushroom"
+    typ = "spectral_ordering_pearson-bfp"#"barycenter-bfp"#"alternating"
+    cislo = i
+    M = np.loadtxt("data/"+slozka+"/"+typ+".csv",
+                    delimiter=",", dtype=int)
+    vstup = copy.deepcopy(M)
+    band, deleted = banded_ones(M, cislo)  
+    print(deleted)
 
-# newcmp = matplotlib.colors.LinearSegmentedColormap.from_list("", ['white','black', 'blue'])
-# newcmp_black_white = matplotlib.colors.LinearSegmentedColormap.from_list("", ['white','black'])
+    # newcmp = matplotlib.colors.LinearSegmentedColormap.from_list("", ['white','black', 'blue'])
+    # newcmp_black_white = matplotlib.colors.LinearSegmentedColormap.from_list("", ['white','black'])
 
-# fig, axs = plt.subplots(1, 3, figsize=(10, 5))
-# axs[0].imshow(vstup, cmap=newcmp_black_white)
-# axs[0].set_title('Original')
+    # fig, axs = plt.subplots(1, 3, figsize=(10, 5))
+    # axs[0].imshow(vstup, cmap=newcmp_black_white)
+    # axs[0].set_title('Original')
 
-# axs[1].imshow(band, cmap=newcmp_black_white)
-# axs[1].set_title('Band')
+    # axs[1].imshow(band, cmap=newcmp_black_white)
+    # axs[1].set_title('Band')
 
-# axs[2].imshow(deleted, cmap=newcmp)
-# axs[2].set_title('Deleting')
+    # axs[2].imshow(deleted, cmap=newcmp)
+    # axs[2].set_title('Deleting')
 
 
-# plt.show()
-np.savetxt("data/paleo/alternating-deleted-band-90-changes.csv", deleted, delimiter=",") 
+    # plt.show()
+    np.savetxt("data/"+slozka+"/"+typ+"-deleted-band-"+ str(cislo) +"-changes.csv", deleted, delimiter=",") 
 
-for i in range(deleted.shape[0]):
-    for j in range(deleted.shape[1]):
-        if deleted[i,j] == 2:
-            deleted[i,j] = 0
+    for i in range(deleted.shape[0]):
+        for j in range(deleted.shape[1]):
+            if deleted[i,j] == 2:
+                deleted[i,j] = 0
 
-np.savetxt("data/paleo/alternating-deleted-band-90.csv", deleted, delimiter=",")         
+    np.savetxt("data/"+slozka+"/"+typ+"-deleted-band-"+ str(cislo) +".csv", deleted, delimiter=",")         
--- final.py
+++ final.py
@@ -13,34 +13,35 @@ from spectral_ordering import spectral_ordering
 from simple_sort import simple_sort
 
 
-M = np.loadtxt("data/zoo/zoo.csv",
+# M = np.loadtxt("data/mushroom/mushroom.csv",
+M = np.loadtxt("data/healthcare/healthcare.csv",
                  delimiter=",", dtype=int)
 
 
 vstup = copy.deepcopy(M)
 bary = spectral_ordering(M)
 banded = bfp(bary)
-print("done")
+# print("done")
 # A, B, factors = GreConD(banded)
 #product = matrix_product(A, B)
 #sim = matrix_similarity(product, banded)
 # print(factors)
 
-newcmp = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", "black"])
-fig, axs = plt.subplots(1, 3, figsize=(15, 10))
-axs[0].imshow(vstup, cmap=newcmp)
-axs[0].set_title('Original')
+# newcmp = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", "black"])
+# fig, axs = plt.subplots(1, 3, figsize=(15, 10))
+# axs[0].imshow(vstup, cmap=newcmp)
+# axs[0].set_title('Original')
 
-axs[1].imshow(bary, cmap=newcmp)
-axs[1].set_title('Spectral Ordering - Pearson s coeficient')
+# axs[1].imshow(bary, cmap=newcmp)
+# axs[1].set_title('Alternating')
 
-axs[2].imshow(banded, cmap=newcmp)
-axs[2].set_title('BFP')
+# axs[2].imshow(banded, cmap=newcmp)
+# axs[2].set_title('BFP')
 
 # axs[3].imshow(B, cmap=newcmp)
 # axs[3].set_title('GreConD - B')
 
-plt.show()
-np.savetxt("data/zoo/spectra-ordering-pearson.csv", bary, delimiter=",")
-np.savetxt("data/zoo/spectra-ordering-pearson-bfp.csv", banded, delimiter=",")
+# plt.show()
+np.savetxt("data/healthcare/spectral_ordering_pearson.csv", bary, delimiter=",")
+np.savetxt("data/healthcare/spectral_ordering_pearson-bfp.csv", banded, delimiter=",")
 # print(factors)
