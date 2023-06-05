7db3be1c931516f379c709aabc692f123d03e067
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-06-05 16:12:40 +0200

    diletation erosion grecon result

--- diletation-erosion.py
+++ diletation-erosion.py
@@ -3,6 +3,7 @@ import matplotlib
 from matplotlib import pyplot as plt
 import copy
 
+from grecond_chatgpt import GreConD
 from diletation import diletation
 from erosion import erosion
 
@@ -19,19 +20,20 @@ mask = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 # amount = [0.2, 0.3, 0.35, 0.4, 0.5]
 for slozka in slozky:
     for typ in typy:            
-        M = np.loadtxt("data/"+slozka+"/"+typ+".csv",
-                        delimiter=",", dtype=int)
-        vstup = copy.deepcopy(M)
-        dilet = diletation(M, mask)
-        eros = erosion(dilet, mask)
-        print(slozka, typ)
-        np.savetxt("data/"+slozka+"/diletation-erosion/"+typ+"/"+typ+"-unit-matrix-3x3.csv", eros, delimiter=",") 
-        # M = np.loadtxt("data/"+slozka+"/square-filter/"+typ+"/"+typ+"-square-filter-"+ str(i) +".csv",
+        # M = np.loadtxt("data/"+slozka+"/"+typ+".csv",
         #                 delimiter=",", dtype=int)
-        # A, B, k = GreConD(M)
-        # np.savetxt("data/"+slozka+"/square-filter/"+typ+"/"+typ+"-square-filter-"+ str(i) +"-grecond-A.csv", A, delimiter=",") 
-        # np.savetxt("data/"+slozka+"/square-filter/"+typ+"/"+typ+"-square-filter-"+ str(i) +"-grecond-B.csv", B, delimiter=",") 
-        # np.savetxt("data/"+slozka+"/square-filter/"+typ+"/"+typ+"-square-filter-"+ str(i) +"-grecond-k.txt", np.array([k]), fmt="%d") 
+        # vstup = copy.deepcopy(M)
+        # dilet = diletation(M, mask)
+        # eros = erosion(dilet, mask)
+        # print(slozka, typ)
+        # np.savetxt("data/"+slozka+"/diletation-erosion/"+typ+"/"+typ+"-unit-matrix-3x3.csv", eros, delimiter=",") 
+        M = np.loadtxt("data/"+slozka+"/diletation-erosion/"+typ+"/"+typ+"-unit-matrix-3x3.csv",
+                        delimiter=",", dtype=int)
+        A, B, k = GreConD(M)
+        print(slozka, typ)
+        np.savetxt("data/"+slozka+"/diletation-erosion/"+typ+"/"+typ+"-unit-matrix-3x3-grecond-A.csv", A, delimiter=",") 
+        np.savetxt("data/"+slozka+"/diletation-erosion/"+typ+"/"+typ+"-unit-matrix-3x3-grecond-B.csv", B, delimiter=",") 
+        np.savetxt("data/"+slozka+"/diletation-erosion/"+typ+"/"+typ+"-unit-matrix-3x3-grecond-k.txt", np.array([k]), fmt="%d") 
 
 # newcmp = matplotlib.colors.LinearSegmentedColormap.from_list("", ['white','black', 'blue', 'green'])
 # newcmp_black_white = matplotlib.colors.LinearSegmentedColormap.from_list("", ['white','black'])
