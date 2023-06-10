70654b61066a29edc4b2c12fc8fb32b7d42b5c02
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-06-10 23:11:59 +0200

    spustitelne asso, divne vysledky

--- asso/asso.py
+++ asso/asso.py
@@ -2,32 +2,27 @@ import numpy as np
 import numpy.matlib
 
 def asso2(M, k, tau, w_p, w_m):
-    # init
     m, n = M.shape
     A = np.zeros((m, n), dtype=bool)
     B = np.zeros((n, m), dtype=bool)
     negM = ~M
 
     # association matrix
-    association_matrix = np.zeros((n, n))
+    association_matrix = np.zeros((n, n), dtype=bool)
 
     for i in range(n):
         for j in range(n):
             if np.sum(np.multiply(M[:, i], M[:, j])) / np.sum(M[:, i]) > tau:
                 association_matrix[i, j] = 1
 
+
     # Asso algorithm
     for factor in range(k):
-        candidate_final_cover = np.zeros(m)
-        final_column = [np.array([], dtype=bool)] * n
+        candidate_final_cover = np.zeros((1,m), dtype=bool)
+        final_column = np.empty((n, m), dtype=bool)
         print(factor)
 
-        # product = np.hstack((A, np.zeros((m, 1), dtype=bool))) @ np.vstack((B, np.zeros((1, n), dtype=bool)))
-        # product = np.logical_and(np.hstack((A, np.zeros((m, 1), dtype=bool))), np.vstack((B, np.zeros((1, n), dtype=bool))))
-        # product = np.logical_and(np.hstack((A, np.zeros((m, 1), dtype=bool))), np.vstack((B, np.zeros((1, n), dtype=bool))))
-        pom1 = np.hstack((A, np.zeros((m, 1), dtype=bool)))
-        pom2 = np.vstack((B, np.zeros((1, n), dtype=bool)))
-        product = np.dot(pom1, pom2)
+        product = np.dot(np.hstack((A, np.zeros((m, 1), dtype=bool))), np.vstack((B, np.zeros((1, n), dtype=bool))))
         product = product.astype(bool)
 
         cover_base = w_p * np.sum(np.sum(M[product])) - w_m * np.sum(np.sum(negM[product]))
@@ -43,19 +38,17 @@ def asso2(M, k, tau, w_p, w_m):
             covered_by_change = np.logical_and(M, changed)
             overcovered_by_change = np.logical_and(negM, changed)
             cover_aktualni = cover_base + w_p * np.sum(covered_by_change, axis=1) - w_m * np.sum(overcovered_by_change, axis=1)
+            
+            final_column[j][cover_base < cover_aktualni] = 1 #kde plati < tam je 1
 
-            final_column[j][cover_base < cover_aktualni] = 1
-            print(A)
-            print(final_column[j])
-            neco1 = np.hstack((A, final_column[j]))
-            neco2 = np.vstack((B, candidate_row))
-            product_final = np.logical_and(neco1, neco2)
-            candidate_final_cover[j] = w_p * np.sum(np.sum(M[product_final])) - w_m * np.sum(np.sum(negM[product_final]))
+            product_final = np.dot(np.column_stack((A, final_column[j])), np.row_stack((B, candidate_row)))
+            product_final = product_final.astype(bool)
+            candidate_final_cover[:,j] = w_p * np.sum(np.sum(M[product_final])) - w_m * np.sum(np.sum(negM[product_final]))
 
         # find the best candidate and add them to A and B
         index = np.argmax(candidate_final_cover)
-        A = np.hstack((A, final_column[index]))
-        B = np.vstack((B, association_matrix[index, :]))
+        A = np.column_stack((A, final_column[index]))
+        B = np.row_stack((B, association_matrix[index, :]))
 
     return A, B
 
