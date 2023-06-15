7e570d27a31572dd7d7c80b94ed79de6651a025b
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-06-15 11:31:24 +0200

    pořádek

--- asso/asso_script.py
+++ asso/asso_script.py
@@ -7,14 +7,14 @@ typy = ["spectral-ordering-pearson-bfp", "barycenter-bfp", "alternating"]
 for slozka in slozky:
     M = np.loadtxt("data/"+slozka+"/"+slozka+".csv",
                         delimiter=",", dtype=int)
-    A, B = asso2(M, 15, 0.9, 1, 1)
+    A, B = asso2(M, 5, 0.9, 1, 1)
     np.savetxt("data/"+slozka+"/"+slozka+"-asso-5-A.csv", A, delimiter=",") 
     np.savetxt("data/"+slozka+"/"+slozka+"-asso-5-B.csv", B, delimiter=",") 
     
-    for typ in typy:
-        M = np.loadtxt("data/"+slozka+"/"+typ+".csv",
-                        delimiter=",", dtype=int)
-        A, B = asso2(M, 5, 0.9, 1, 1)
-        np.savetxt("data/"+slozka+"/"+typ+"-asso-5-A.csv", A, delimiter=",") 
-        np.savetxt("data/"+slozka+"/"+typ+"-asso-5-B.csv", B, delimiter=",") 
+    # for typ in typy:
+    #     M = np.loadtxt("data/"+slozka+"/"+typ+".csv",
+    #                     delimiter=",", dtype=int)
+    #     A, B = asso2(M, 5, 0.9, 1, 1)
+    #     np.savetxt("data/"+slozka+"/"+typ+"-asso-5-A.csv", A, delimiter=",") 
+    #     np.savetxt("data/"+slozka+"/"+typ+"-asso-5-B.csv", B, delimiter=",") 
             
