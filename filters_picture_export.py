f332f1d761ddd11d1e51f1b39dde8ad3fcdb7fb4
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-07-03 12:50:55 +0200

    deleted band pictures

--- filters_picture_export.py
+++ filters_picture_export.py
@@ -3,8 +3,8 @@ from matplotlib import pyplot as plt
 import matplotlib
 
 types = ["spectral-ordering-pearson-bfp", "barycenter-bfp", "alternating", "barycenter", "barycenter-bfp-alternating"]
-filters = [("deleted-band", ["30", "50", "70", "90"]), ("square-filter",["0.2", "0.3", "0.4", "0.5", "0.35"] ), ("diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("diletation-erosion-erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("erosion-diletation-diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"])]
-folders = ["mushroom"] #"mushroom" export zvlast kvuli roztazeni , "paleo", "zoo", "healthcare"
+filters = [("deleted-band", ["30", "50", "70", "90"])] #, ("square-filter",["0.2", "0.3", "0.4", "0.5", "0.35"] ), ("diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("diletation-erosion-erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("erosion-diletation-diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"])
+folders = ["paleo", "zoo", "healthcare"] #"mushroom" export zvlast kvuli roztazeni , "paleo", "zoo", "healthcare"
 newcmp_black_white = matplotlib.colors.LinearSegmentedColormap.from_list("", ['white','black'])
 newcmp = matplotlib.colors.LinearSegmentedColormap.from_list("", ['white','black', 'blue'])
 
