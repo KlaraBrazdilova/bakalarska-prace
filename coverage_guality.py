1b67510e315229f972c737257e1ec0764a2d8ab8
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-07-21 11:05:41 +0200

    zoo grcond graph

--- coverage_guality.py
+++ coverage_guality.py
@@ -5,20 +5,8 @@ import matplotlib
 from matrix_similarity import matrix_similarity
 from matrix_product import matrix_product
 
-A = np.loadtxt("data/zoo/grecond-chat-A-alternating-deleted-band-30.csv",
-                 delimiter=",", dtype=int)
-B = np.loadtxt("data/zoo/grecond-chat-B-alternating-deleted-band-30.csv",
-                 delimiter=",", dtype=int)
-I = np.loadtxt("data/zoo/alternating.csv", delimiter=",", dtype=int)
-
-# product = matrix_product(A, B)
-coverage = [0] #pridat 0 na začátek
-for i in range(1, I.shape[0]):
-    coverage.append(matrix_similarity(matrix_product(A[:,0:i], B[0:i,:]), I))
-        
-print(coverage)
-print(range(0,I.shape[0]))
-fig, axs = plt.subplots(1, 1, figsize=(5, 5))
-axs.plot(range(0,I.shape[0]), coverage)
-plt.tight_layout()
-plt.show()
+def coverage_guality(A, B, I):
+    coverage = [] #pridat 0 na začátek
+    for i in range(1, I.shape[0]):
+        coverage.append(matrix_similarity(matrix_product(A[:,0:i], B[0:i,:]), I))
+    return coverage
--- learning_numpy.py
+++ learning_numpy.py
@@ -1,10 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-B = np.matrix([[0, 0, 1, 1, 1, 1], [0, 1, 0, 1, 0, 1]])
-[m, n] = B.shape
-indexes_rows = np.arange(m)
-indexes_cols = np.arange(n)
-print(indexes_cols)
-for i in range(B.shape[0]):
-    print(indexes_cols.shape, B[i].shape)
-    print(np.multiply(B[i], indexes_cols))
+x = np.arange(0, 1, 0.1)  # Generate x values
+y1 = np.sin(x)  # Generate y values using algorithm 1
+# y2 = np.cos(x)  # Generate y values using algorithm 2
+print(x)
+y1 = [1500, 1520, 1530, 1590, 1570, 1550, 1530, 1510, 1580, 1600]
+plt.plot(x, y1, label='Algorithm 1')
+# plt.plot(x, y2, label='Algorithm 2')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Comparison of Algorithms')
+plt.legend()
+
+plt.show()
