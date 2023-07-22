bb7a97c77c4ad20a70e883ec6491d3679e544410
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-07-22 12:01:57 +0200

    outputs

--- coverage_guality.py
+++ coverage_guality.py
@@ -10,3 +10,20 @@ def coverage_guality(A, B, I):
     for i in range(1, I.shape[0]):
         coverage.append(matrix_similarity(matrix_product(A[:,0:i], B[0:i,:]), I))
     return coverage
+
+# def coverage_guality_2(product, I):
+#     coverage = [] #pridat 0 na začátek
+#     for i in range(1, I.shape[0]):
+#         coverage.append(matrix_similarity(product, I))
+#     return coverage
+
+# A = np.loadtxt("data/healthcare/alternating/square-filter/GreConD/alternating-square-filter-0.3-grecond-A.csv",
+#                              delimiter=",", dtype=int)
+# B = np.loadtxt("data/healthcare/alternating/square-filter/GreConD/alternating-square-filter-0.3-grecond-B.csv",
+#                              delimiter=",", dtype=int)
+# I = np.loadtxt("data/healthcare/alternating/square-filter/alternating-square-filter-0.3.csv", delimiter=",", dtype=int)
+# product = np.loadtxt("data/healthcare/alternating/square-filter/GreConD/alternating-square-filter-0.3-grecond-product.csv", 
+#                      delimiter=",", dtype=int)
+# print("nacteno")
+# print(coverage_guality_2(product,I))
+# print(coverage_guality(A,B,I))
