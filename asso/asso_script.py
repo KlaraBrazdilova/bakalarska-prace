d664a6ad11959d17a5b20ccded6f8aa70cf714bb
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-07-28 15:15:37 +0200

    asso progress

--- asso/asso_script.py
+++ asso/asso_script.py
@@ -1,8 +1,8 @@
 import numpy as np
 from asso import asso2
 
-files = ["paleo", "zoo", "mushroom", "healthcare"]
-types = ["spectral-ordering-pearson-bfp", "barycenter-bfp", "alternating"]
+files = ["zoo", "healthcare"] #"mushroom", "paleo"
+types = ["spectral-ordering-pearson-bfp-fix", "barycenter-bfp", "alternating", "barycenter", "barycenter-bfp-alternating"]
 filters = [("deleted-band", ["30", "50", "70", "90"]), ("square-filter",["0.2", "0.3", "0.4", "0.5", "0.35"] ), ("diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("diletation-erosion-erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("erosion-diletation-diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"])]
 factors = [5, 10, 15]
 
@@ -15,7 +15,7 @@ for file in files:
                 for factor in factors:
                     M = np.loadtxt("data/"+file+"/"+type+"/"+filter_name+"/"+type+"-"+filter_name+"-"+str(amount)+".csv",
                                         delimiter=",", dtype=int)
-                    A, B = asso2(M, 5, 0.9, 1, 1)
-                    np.savetxt("data/"+file+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-"+str(amount)+"-ASSO-A.csv", A, delimiter=",") 
-                    np.savetxt("data/"+file+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-"+str(amount)+"-ASSO-B.csv", B, delimiter=",") 
+                    A, B = asso2(M, factor, 0.9, 1, 1)
+                    np.savetxt("data/"+file+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-"+str(amount)+"-asso-"+str(factor)+"-A.csv", A, delimiter=",") 
+                    np.savetxt("data/"+file+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-"+str(amount)+"-asso-"+str(factor)+"-B.csv", B, delimiter=",") 
                 
