c261fdd16c62cadc53bce85e796b6a4ec049a882
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-07-31 12:10:34 +0200

    fixed graphs

--- coverage-graph-asso.py
+++ coverage-graph-asso.py
@@ -5,14 +5,14 @@ import matplotlib
 from coverage_guality import coverage_guality
 from matrix_similarity import matrix_similarity
 
-types = ["barycenter-bfp"]#, "alternating", "barycenter", "barycenter-bfp-alternating", "spectral-ordering-pearson-bfp-fix"]
+types = ["barycenter-bfp", "alternating", "barycenter", "barycenter-bfp-alternating", "spectral-ordering-pearson-bfp-fix"]
 filters = [("square-filter",["0.2", "0.3", "0.4", "0.5", "0.35"] ), 
            ("diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
            ("erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
            ("diletation-erosion-erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
            ("erosion-diletation-diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
            ("deleted-band", ["30", "50", "70", "90"])]
-folders = ["zoo"] #"mushroom" export zvlast kvuli roztazeni , "paleo", "zoo", "healthcare"
+folders = ["healthcare"] #"mushroom" export zvlast kvuli roztazeni , "paleo", "zoo", "healthcare"
 factors = ["5", "10", "15"]
 
 for factor in factors:
@@ -37,8 +37,8 @@ for factor in factors:
                 plt.ylabel('Pokrytí', fontsize="15")
                 plt.title('Coverage of' + type)
                 plt.legend(fontsize="15")
-                #plt.savefig("data/"+folder+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-asso"+factor+"-coverage.png", bbox_inches='tight')        
-                plt.show()
+                plt.savefig("data/"+folder+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-asso"+factor+"-coverage.png", bbox_inches='tight')        
+                #plt.show()
                 matplotlib.pyplot.close()
                 print("graph done")
 
