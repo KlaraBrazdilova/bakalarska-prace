01b6953d547341dbe26fa07a199d0407dca16a41
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-08-02 00:10:14 +0200

    sim

---
+++ similarity_table.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from matrix_similarity import matrix_similarity
+
+types = ["spectral-ordering-pearson-bfp-fix", "barycenter-bfp", "alternating", "barycenter", "barycenter-bfp-alternating"]
+filters = [("square-filter",["0.2", "0.3", "0.4", "0.5", "0.35"] ), 
+           ("deleted-band", ["30", "50", "70", "90"]), 
+           ("diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
+           ("erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
+           ("diletation-erosion-erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
+           ("erosion-diletation-diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"])]
+folders = ["paleo", "zoo", "healthcare", "mushroom"] 
+
+# permuted = np.loadtxt("data/zoo/alternating.csv", delimiter=",", dtype=int)
+# filtered = np.loadtxt("data/zoo/alternating/square-filter/alternating-square-filter-0.2.csv", delimiter=",", dtype=int)
+output = []
+for folder in folders:
+    for type in types:
+        permuted = np.loadtxt("data/"+folder+"/"+type+"/"+type+".csv", delimiter=",", dtype=int)
+        line = [folder+"-"+type]
+        for filter in filters:
+            filter_name, filter_amount = filter
+            line.append(filter_name)
+            for amount in filter_amount:
+                filtered = np.loadtxt("data/"+folder+"/"+type+"/"+filter_name+"/"+type+"-"+filter_name+"-"+amount+".csv", delimiter=",", dtype=int)
+                a = str(np.around(matrix_similarity(permuted, filtered), 2))
+                # print(a)
+                line.append(a)
+                # print(folder + "-" + type + "-" + filter_name + "-" + amount + ": " + str(matrix_similarity(permuted, filtered)))
+        print(line)
+        output.append(line)
+
+np.savetxt("similarity.csv", output, delimiter=",", fmt='%s')
