47f14bba7a829f63b24a36c9052b4d23e1a53188
KlaraBrazdilova <91657069+KlaraBrazdilova@users.noreply.github.com>
2023-08-05 19:04:44 +0200

    asso

--- asso/asso_script.py
+++ asso/asso_script.py
@@ -2,21 +2,33 @@ import numpy as np
 from asso import asso2
 
 #barycenter-bfp-deleted-band-paleo
-files = ["healthcare"] #"mushroom", "paleo"
-types = ["barycenter-bfp"]#"spectral-ordering-pearson-bfp-fix", "alternating", "barycenter", "barycenter-bfp-alternating"
-filters = [("deleted-band", ["30", "50", "70", "90"])]#("square-filter",["0.2", "0.3", "0.4", "0.5", "0.35"])]#,# ("diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("diletation-erosion-erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), ("erosion-diletation-diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"])]
+files = ["healthcare", "zoo", "paleo", "mushroom"] #"mushroom", "paleo"
+types = ["barycenter-bfp", "spectral-ordering-pearson-bfp", "alternating", "barycenter", "barycenter-bfp-alternating"]
+filters = [("deleted-band", ["30", "50", "70", "90"]), 
+           ("square-filter",["0.2", "0.3", "0.4", "0.5", "0.35"]),
+            ("diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
+            ("erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
+            ("diletation-erosion-erosion-diletation",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"]), 
+            ("erosion-diletation-diletation-erosion",["col-matrix-3x3", "col-matrix-3x2", "unit-matrix-3x3"])]
 factors = [5, 10, 15]
 
 
 for file in files:
     for type in types:
-        for filter in filters:
-            filter_name, filter_amount = filter
-            for amount in filter_amount:
-                for factor in factors:
-                    M = np.loadtxt("data/"+file+"/"+type+"/"+filter_name+"/"+type+"-"+filter_name+"-"+str(amount)+".csv",
-                                        delimiter=",", dtype=int)
-                    A, B = asso2(M, factor, 0.9, 1, 1)
-                    np.savetxt("data/"+file+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-"+str(amount)+"-asso-"+str(factor)+"-A.csv", A, delimiter=",") 
-                    np.savetxt("data/"+file+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-"+str(amount)+"-asso-"+str(factor)+"-B.csv", B, delimiter=",") 
+        for factor in factors:
+            M = np.loadtxt("data/"+file+"/"+type+"/"+type+".csv",
+                                delimiter=",", dtype=int)
+            A, B = asso2(M, factor, 0.9, 1, 1)
+            np.savetxt("data/"+file+"/"+type+"/"+type+"-asso-"+str(factor)+"-A.csv", A, delimiter=",") 
+            np.savetxt("data/"+file+"/"+type+"/"+type+"-asso-"+str(factor)+"-B.csv", B, delimiter=",") 
+              
+        # for filter in filters:
+        #     filter_name, filter_amount = filter
+        #     for amount in filter_amount:
+        #         for factor in factors:
+        #             M = np.loadtxt("data/"+file+"/"+type+"/"+filter_name+"/"+type+"-"+filter_name+"-"+str(amount)+".csv",
+        #                                 delimiter=",", dtype=int)
+        #             A, B = asso2(M, factor, 0.9, 1, 1)
+        #             np.savetxt("data/"+file+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-"+str(amount)+"-asso-"+str(factor)+"-A.csv", A, delimiter=",") 
+        #             np.savetxt("data/"+file+"/"+type+"/"+filter_name+"/ASSO/"+type+"-"+filter_name+"-"+str(amount)+"-asso-"+str(factor)+"-B.csv", B, delimiter=",") 
                 
